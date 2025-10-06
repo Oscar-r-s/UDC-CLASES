6a4255886fb53f52f65206bfe49746a30810c262
Oscar-r-s <oscars-email@protonmail.com>
2025-10-06 10:07:03 +0200

    06/10/2025 -> (2)

--- Informatica/EjerciciosClase/Semana 3/ejercicio2.py
+++ Informatica/EjerciciosClase/Semana 3/ejercicio2.py
@@ -8,12 +8,14 @@ factorial = n*(n-1)*(n-2)*....*1
 • Es obligatorio el uso de for y range para resolver el ejercicio
 """
 
+print("============================================= ")
 n = int(input("Introduce un número entero positivo: "))
 
 factorial = n
 num_i = 0
 
 while n<=0 :
+    print("============================================= ")
     print("Error en el rango")
     n = int(input("Introduce un número entero positivo: "))
 
@@ -24,4 +26,5 @@ for i in range(1, n):
     #Actualizo el valor de la variable factorial
     factorial = factorial * num_i
 
-print(factorial)
+print(f"Factorial de {n} : {n}! = {factorial}")
+print("============================================= ")
