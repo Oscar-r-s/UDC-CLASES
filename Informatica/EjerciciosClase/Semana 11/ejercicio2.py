4ac071e7d5e78ba54b626a3480b0880bca2b3717
Oscar <oscars-email@protonmail.com>
2026-01-14 17:17:17 +0100

    14/01/2026 -> (2)

--- Informatica/EjerciciosClase/Semana 11/ejercicio2.py
+++ Informatica/EjerciciosClase/Semana 11/ejercicio2.py
@@ -1,7 +1,10 @@
 
 def menu_y_opcion(opciones_posibles, equipo):
     print(
-    '''  Menú de opciones: 
+    '''  
+    ---------------------------------------------------------------------
+
+        Menú de opciones: 
         (1) Añadir jugador 
         (2) Eliminar jugador 
         (3) Mostrar jugador 
@@ -11,7 +14,13 @@ def menu_y_opcion(opciones_posibles, equipo):
     )
     opcion_escogida = int(input("   Elige una opción : "))
 
-    return opciones_posibles[opcion_escogida](equipo)
+    if opcion_escogida == 6 :
+        print("---     PROGRAMA TERMINADO       ---")
+        return
+
+    else :
+        opciones_posibles[opcion_escogida](equipo)
+        menu_y_opcion(opciones_posibles, equipo)
 
 def add_jugador(equipo):
     dorsal = int(input("Introduce el dorsal del jugador : "))
@@ -22,6 +31,13 @@ def add_jugador(equipo):
     nombre = input("Introduce el nombre del jugador : ")
 
     equipo[dorsal] = nombre
+
+def eliminar_jugador(equipo):
+    dorsal = int(input("Introduce el dorsal del jugador a eliminar: "))
+
+    while dorsal not in equipo :
+        dorsal = int(input("Dorsal inexistente. Introduce un dorsal que SÍ esté en el equipo : "))
+    equipo.pop(dorsal)
 
 def mostrar_jugador(equipo):
     dorsal = int(input("Introduce el dorsal : "))
@@ -34,37 +50,42 @@ def listar_plantilla(equipo):
     for clave, valor in equipo.items() :
         print(f"{clave}     {valor}")
 
+def crear_convocatoria(equipo):
+    convocatoria = {}
+    numero_jugadores = 12
+    print(f"Introduzca el dorsal de los {numero_jugadores} jugadores convocados :")
+
+    for i in range(numero_jugadores + 1 ):
+        dorsal = int(input("Introduce el dorsal : "))
+
+        while dorsal not in equipo :
+            dorsal = int(input("El dorsal no existe, inténtalo de nuevo : "))
+
+        convocatoria[dorsal] = equipo[dorsal]
+    
+    print('\nJugadores convocados : ')
+
+    for clave, valor  in convocatoria.items() :
+        print(f"{valor}")
+
 #---------------------------------------------------------------------------------------
 def main():
     opciones = {
         1 : add_jugador,
-        #2 : eliminar_jugador,
+        2 : eliminar_jugador,
         3 : mostrar_jugador,
         4 : listar_plantilla,
-        # 5 : crear_convocatoria,
+        5 : crear_convocatoria,
         # 6 : terminar
     } 
-
     plantilla = {
-        0 : "Williams-Goss",
-        1 : "Causeur",
-        3 : "Randolph",
-        5 : "Rudy",
-        6 : "Abalde",
-        8 : "Hanga",
-        11 : "Hezonja",
-        12 : "Alocén",
-        13 : "Rodríguez",
-        14 : "Deck",
-        17 : "Poirier",
-        21 : "Cornelie",
-        22 : "Tavares",
-        23 : "Llull",
-        28 : "Yabusele",
-        30 : "Ndiaye"
+        0 : "Williams-Goss", 1 : "Causeur", 3 : "Randolph", 5 : "Rudy",
+        6 : "Abalde", 8 : "Hanga", 11 : "Hezonja", 12 : "Alocén",
+        13 : "Rodríguez", 14 : "Deck", 17 : "Poirier", 21 : "Cornelie",
+        22 : "Tavares", 23 : "Llull", 28 : "Yabusele", 30 : "Ndiaye"
     }
-
     opcion_escogida = menu_y_opcion(opciones, plantilla)
+
 
 if __name__ == "__main__":
     main()
